dd5047e5ca6bb5d542e0e570fbe9e1d014053725
Henok Asaye <henokasaye77@gmail.com>
2025-04-24 11:49:42 +0300

    add the default input decoration to the main file

--- app/frontend/lib/main.dart
+++ app/frontend/lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/features/auth/pages/signup.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task Manager',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3:true,
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.all(20),
+                enabledBorder:OutlineInputBorder(
+                    borderSide:BorderSide(
+                      color:Colors.grey.shade300,
+                      width:2
+                    ),
+                    borderRadius:BorderRadius.circular(10)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide( width:1.5)
+                )
+              ),
+              useMaterial3: true
       ),
-      home:const SignUpPage()
+      home: const SignUpPage()
     );
   }
 }
